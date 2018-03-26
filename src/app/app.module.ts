884953cd0784f2ecf98a5dee7a922e80e7e84b61
jentr3s <jentr3s03@gmail.com>
2018-03-26 17:33:42 +0800

    created new nodeproject project with routing, sass, bootstrap and fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
