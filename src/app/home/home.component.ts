0b2809099b6de43beef732997970e88ecc79cc67
jentr3s <jentr3s03@gmail.com>
2018-03-29 20:00:13 +0800

    wip  : working on quizzes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'; 
+import { Router } from '@angular/router';
 import { SharedService } from '../shared.service';
+
+declare let electron: any;
 
 @Component({
   selector: 'app-home',
@@ -10,18 +12,35 @@ import { SharedService } from '../shared.service';
 export class HomeComponent implements OnInit {
   public showMainPage: boolean = false;
 
+  // IPC Renderer
+  ipc = electron.ipcRenderer;
+  quizzes: any;
+  items: any;
+
   constructor(private router: Router, private sharedService: SharedService) { }
 
   ngOnInit() {
     this.sharedService.mainPage.subscribe(res => this.showMainPage = res);
     this.sharedService.changeMainPage(this.showMainPage);
+
+    this.loadQuizzes()
   }
 
-  back(){
+  loadQuizzes() {
+    let result = this.ipc.sendSync("loadQuizzes")
+    this.quizzes = JSON.parse(result);
+  }
+
+  loadItems(quizId: number) {
+    let result = this.ipc.sendSync("loadItems", quizId)
+    this.items = JSON.parse(result);
+  }
+
+  back() {
     this.sharedService.changeMainPage(this.showMainPage = true);
-    
+
     this.router.navigate(['']);
-    
+
   }
 
 }
